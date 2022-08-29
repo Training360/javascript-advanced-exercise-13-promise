f094f9f97d590225141a9265b9f413f039f5cbf7
Pryann <pryann@gmail.com>
2022-08-29 15:19:03 +0200

    update repo

--- 13-promise/exercises/01/solution/js/convertToUppercase.js
+++ 13-promise/exercises/01/solution/js/convertToUppercase.js
@@ -1,10 +1,1 @@
-const convertToUppercase = (arr) => new Promise((resolve, reject) => {
-  try {
-    const uppercaseStringsArray = arr.map((str) => str.toLocaleUpperCase());
-    resolve(uppercaseStringsArray);
-  } catch (error) {
-    reject(new Error('Error: Not all items in the array are strings!'));
-  }
-});
-
 export default convertToUppercase;
